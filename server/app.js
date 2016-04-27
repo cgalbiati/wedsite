2c58047ff481248f1d817798a433304ff47014ce
Chandra Galbiati <chandra.galbiati@gmail.com>
2016-04-27 00:15:21 -0400

    finished story pg

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
+// var session = require('express-session');
 var debug = require('debug')('wedsite:server');
 var http = require('http');
 var app = express();
@@ -15,19 +15,19 @@ var app = express();
  * Configure app
  */
 app.use(logger('dev'));
-app.use(session({
-    // this mandatory configuration ensures that session IDs are not predictable
-    secret: 'galbathomas'
-}));
+// app.use(session({
+//     // this mandatory configuration ensures that session IDs are not predictable
+//     secret: 'galbathomas'
+// }));
 app.use(cookieParser());
 // Parse our POST and PUT bodies.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(function (req, res, next) {
-  if (!req.session.userId) req.session.userId = Math.random();
-  next();
-});
+// app.use(function (req, res, next) {
+//   if (!req.session.userId) req.session.userId = Math.random();
+//   next();
+// });
 
 //make static routes for files in public and node_modules
 app.use(express.static(path.join(__dirname, '../browser/public')));  
@@ -63,8 +63,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.send('error', {
+    res.status(err.status || 500).send('error', {
       message: err.message,
       error: err
     });
@@ -74,8 +73,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.send('error', {
+  res.status(err.status || 500).send('error', {
     message: err.message,
     error: {}
   });
